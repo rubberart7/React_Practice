4c2ac05db5d47ffcaaf97c0553c9c7e6fb7e2b5f
mbyaf <rubberart7@users.noreply.github.com>
2025-05-18 21:43:45 -0400

    useEffect finished

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -2,31 +2,36 @@ import React, {useState, useEffect} from "react";
 
 function MyComponent() {
 
-    const [count, setCount] = useState(0);
-    const [color, setColor] = useState("green");
+    const [width, setWidth] = useState(window.innerWidth);
+    const [height, setHeight] = useState(window.innerHeight);
 
     useEffect(() => {
-        document.title = `Count: ${count} ${color}`
-    }, [count, color]);
+        window.addEventListener("resize", handleResize);
+        console.log("Event listener added.")
 
-    function addCount() {
-        setCount(c => c + 1);
+        return () => {
+            window.removeEventListener("resize", handleResize);
+            console.log("Event listener removed")
+        }
+
+    }, [])
+
+    useEffect(() => {
+        document.title = `Size: ${width} x ${height}`;
+    }, [width, height]);
+
+    function handleResize() {
+        setWidth(window.innerWidth);
+        setHeight(window.innerHeight);
     }
 
-    function subtractCount() {
-        setCount(c => c - 1);
-    }
-
-    function changeColor() {
-        setColor(c => c === "green" ? "red" : "green");
-    }
+    // window.addEventListener("resize", handleResize);
+    // if you use this method you will conintously add event listeners each time your page renders
 
     return (
         <>
-            <p style={{color: color}}>Count: {count}</p>
-            <button onClick={addCount}>Add</button>
-            <button onClick={subtractCount}>Subtract</button><br />
-            <button onClick={changeColor}>Change Color</button>
+            <p>Window width: {width} px</p>
+            <p>Window height: {height} px</p>
         </>
     )
 }
